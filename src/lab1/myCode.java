3c359169c73a94a4fa14910d970f2c84eacf6c08
s361211760018 <zeejaruwan27@gmail.com>
2018-06-26 11:40:03 +0700

    Hello World

---
+++ src/lab1/myCode.java
@@ -0,0 +1,27 @@
+package lab1;
+
+public class myCode {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world.");
+        System.out.println("จารุวรรณ ปัญญาจุติ");
+        System.out.println("361211760018");
+        System.out.println("MIT211");
+        System.out.println("RMUTSV");
+
+
+
+
+        
+
+    }//main
+
+
+
+
+
+
+
+}//class
